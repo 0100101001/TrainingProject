fea91fa87555aac8353717c58275737bb6a83a48
amoRNizam <mazin.roma1992@yandex.ru>
2019-04-05 23:00:18 +0300

    Ошибка io.qameta.atlas.core.AtlasException: URI WebSite did'nt declared.

--- src/test/java/loginTests/TestLogin.java
+++ src/test/java/loginTests/TestLogin.java
@@ -45,6 +45,37 @@ public class TestLogin extends TestBase {
         onSite().onMyAccountPage().loginCheck();
     }
 
+    @Test(description = "Проверка авторизации с некорректными данными")
+    public void incorrectDataLoginTest() {
+        LoginAndPassword loginAndPassword = new LoginAndPassword();
+
+        String login = loginAndPassword.invalidLogin;
+        String password = loginAndPassword.invalidPassword;
+
+        // Перейти на сайт
+        onSite().openSite();
+
+        // Перейти на страницу авторизации
+        onSite().onMainPage().goToLoginPage();
+
+        // Проверить, что открыта страница авторизации
+        onSite().onLoginPage().checkThatTheLoginPageIsOpen();
+
+        // Проверить, что отображается форма авторизации
+        onSite().onLoginPage().verifyThatTheLoginformIsDisplayed();
+
+        // Ввести логин
+        onSite().onLoginPage().inputLogin(login);
+
+        // Ввести пароль
+        onSite().onLoginPage().inputPassword(password);
+
+        // Нажать кнопку завершения авторизации
+        onSite().onLoginPage().pressSubmitButton();
+
+        // Проверить, что авторизация не удалась - отображается сообщение об ошибке
+        onSite().onLoginPage().checkNotificationError();
+    }
     private PageObject onSite() {
         return atlas.create(webDriver, PageObject.class);
     }
