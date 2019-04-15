8d00e3bcf5feae9f3ceb765cdf965e43f95cfd4f
amoRNizam <mazin.roma1992@yandex.ru>
2019-04-15 17:47:10 +0300

    Рефакторинг.

--- src/main/java/pages/myAccountPage/MyAccountPage.java
+++ src/main/java/pages/myAccountPage/MyAccountPage.java
@@ -4,9 +4,10 @@ import io.qameta.atlas.webdriver.AtlasWebElement;
 import io.qameta.atlas.webdriver.WebPage;
 import io.qameta.atlas.webdriver.extension.FindBy;
 import org.testng.Assert;
+import pages.loginPage.LoginPage;
 import ru.yandex.qatools.allure.annotations.Description;
 
-public interface MyAccountPage extends WebPage {
+public interface MyAccountPage extends WebPage, LoginPage {
 
     @Description("Аватар пользователя")
     @FindBy("//div[@class= 'header-main-area']//span[@class='my-account-personal-photo-placeholder']")
@@ -14,5 +15,7 @@ public interface MyAccountPage extends WebPage {
 
     default void loginCheck() {
         Assert.assertTrue(userAvatar().isDisplayed(), "Аватар пользователя не отображается");
+        Assert.assertFalse(getWrappedDriver().getCurrentUrl().contains("login"),
+                "Открыта страница авторизации" + getWrappedDriver().getCurrentUrl());
     }
 }
